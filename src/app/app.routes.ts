6fc6f376109b802f11802ac63d7658e6d0f68cf2
Muhammad Bilal <mbilal2569@gmail.com>
2024-07-17 12:21:25 +0500

    installing Angular Material UI and Tailwind CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [
-    {
-        path: 'dashboard',
-        component: DashboardComponent,
-    }
-];
+export const routes: Routes = [];
